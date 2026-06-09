24bf32403cb4dccce8e20dad10d2e45b5aca89a8
vaqsi1990 <vaqsii23@gmail.com>
2026-06-09 21:22:27 +0400

    tours

--- components/ToursPage.tsx
+++ components/ToursPage.tsx
@@ -19,14 +19,7 @@ export default async function ToursPage({ destination }: ToursPageProps = {}) {
   return (
     <main className="bg-white px-4 pb-20 pt-28 text-black sm:px-6 sm:pb-24 sm:pt-32 lg:px-10 lg:pb-28 lg:pt-36">
       <div className="mx-auto w-full max-w-7xl">
-        <Image
-          src="/background.png"
-          className="mx-auto object-cover"
-          alt={title}
-          width={300}
-          height={100}
-        />
-
+       
         <div className="mb-10 text-center sm:mb-14">
           <h1 className="font-afacad mb-3 text-3xl font-semibold tracking-tight text-black sm:text-4xl lg:text-[2.5rem]">
             {title}
